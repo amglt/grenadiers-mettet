041bdc1bd9942351488d3a026a102ec76ef97dd0
Amglt <a.mergalet@protonmail.com>
2024-02-14 11:34:07 +0100

    feat: Logo

---
+++ src/components/Container/Container.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	export let classes: string;
+</script>
+
+<div class={`px-7.5 mx-auto my-0 max-w-[980px] ${classes}`}>
+	<slot />
+</div>
--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,5 +1,14 @@
+<script>
+	import Container
+		from '../Container/Container.svelte';
+	import Logo
+		from './Logo.svelte';
+</script>
+
 <div class="relative">
 	<div class="relative w-full z-10">
-
+		<Container classes="h-14">
+			<Logo />
+		</Container>
 	</div>
 </div>
