b0ced1b54d0e29d6e4a0591a4030cae7b3137bb8
Adrien Mergalet <a.mergalet@protonmail.com>
2024-07-16 17:07:42 +0200

    feat: app menu behavior should vary between mobile and desktop

--- src/components/Header/Logo.svelte
+++ src/components/Header/Logo.svelte
@@ -1,5 +1,5 @@
-<div class="font-raleway float-left text-header-logo text-base tracking-wide uppercase w-auto">
-	<h1 class="relative text-header-text top-[17px] font-semibold tracking-[1.4px]">
+<div class="font-raleway text-header-logo text-base tracking-wide uppercase w-auto">
+	<h1 class="text-header-text font-semibold tracking-[1.4px]">
 		Les Grenadiers
 	</h1>
 </div>
--- src/components/Menu/Menu.svelte
+++ src/components/Menu/Menu.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
-	import {
-		Hamburger
-	} from 'svelte-hamburgers';
-	import MenuLink
-		from './MenuLink.svelte';
-
-	export let open: boolean;
+	import MenuLink from './MenuLink.svelte';
 </script>
 
-<div class="float-right w-auto">
-	<div class="lg:hidden"><Hamburger bind:open /></div>
-	<div class="clear-both"></div>
+<div class="text-lg">
+	<MenuLink to="/">Accueil</MenuLink>
+	<MenuLink to="#">Prestations</MenuLink>
+	<MenuLink to="#">Blog</MenuLink>
 </div>
-
-{#if open}
-	<div class="text-center text-2xl p-4 pt-0">
-		<MenuLink to="/">Home</MenuLink>
-	</div>
-
-	<div class="border border-white" />
-{/if}
